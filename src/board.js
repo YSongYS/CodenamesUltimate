4f8e64aeada73b32e16efaf66d40b53745bfdc68
YSongYS <songyishu.yang@gmail.com>
2019-03-01 11:14:52 +0000

    create timer

--- src/board.js
+++ src/board.js
@@ -2,6 +2,7 @@ class Board{
   constructor(){
     this.cards = []
     this.startColor = this.initStartColor()
+    this.materView = this.initNavToggle()
     this.boardEl = document.querySelector('div.board-container')
   }
 
@@ -15,6 +16,34 @@ class Board{
   initStartColor(){
     if (Math.round(Math.random())===1) {return 'red'}
     else {return 'blue'}
+  }
+
+  initNavToggle(){
+    const operativeViewBtn = document.querySelector('#player-view-btn')
+    const masterViewBtn = document.querySelector('#master-view-btn')
+    operativeViewBtn.addEventListener('click',this.operativeView.bind(this))
+    masterViewBtn.addEventListener('click',this.masterView.bind(this))
+    const startTimerBtn = document.querySelector('#start-timer-btn')
+    startTimerBtn.addEventListener('click', this.timer.bind(this))
+    return false
+  }
+
+  operativeView(){
+    this.renderBoard(this.cards)
+    const operativeViewBtn = document.querySelector('#player-view-btn')
+    const masterViewBtn = document.querySelector('#master-view-btn')
+    operativeViewBtn.classList = "btn btn-info"
+    masterViewBtn.classList = "btn btn-secondary"
+  }
+
+  masterView(){
+    this.cards.forEach(cardInstance => {
+      cardInstance.showCard()
+    })
+    const operativeViewBtn = document.querySelector('#player-view-btn')
+    const masterViewBtn = document.querySelector('#master-view-btn')
+    operativeViewBtn.classList = "btn btn-secondary"
+    masterViewBtn.classList = "btn btn-info"
   }
 
   initTypeArray(){
@@ -44,14 +73,37 @@ class Board{
   }
 
   renderBoard(cards){
+    this.boardEl.innerHTML = ''
     cards.forEach((cardInstance) => {
       this.boardEl.appendChild(cardInstance.getElement())
+      if (cardInstance.clicked) {cardInstance.showCard()}
     })
   }
 
   createCard(card){
     const cardInstance = new Card (card)
     this.cards.push(cardInstance)
+  }
+
+  timer(){
+    let timePause = false
+    const timerEl = document.querySelector('div.countdown p')
+    console.log(timerEl.innerText)
+    timerEl.innerText = "03:00";
+
+    let countDown = setInterval(getTime, 1000)
+
+    function getTime (){
+      let [min,sec] = timerEl.innerText.split(":")
+      let secondsLeft = parseInt(min)*60 + parseInt(sec)
+      secondsLeft -= 1;
+      min = Math.floor(secondsLeft/60)
+      sec = secondsLeft%60
+      sec = (sec<10)? `0${sec}`: sec
+      min = (min<10)? `0${min}`: min
+      timerEl.innerText = `${min}:${sec}`
+    }
+
   }
 
 }
--- src/card.js
+++ src/card.js
@@ -3,29 +3,27 @@ class Card{
     this.id = card.id
     this.word = card.word
     this.type = card.type
+    this.clicked = false
   }
 
   getElement(card){
     const cardEl = document.createElement('div')
     cardEl.classList = 'card flex'
     cardEl.dataset.id = this.id
-    cardEl.addEventListener('click', e => {this.showColor(e)})
-
-    cardEl.innerText = `${this.word}`
-
-    // const wordEl = document.createElement('span')
-    // wordEl.innerText = `${this.word}`
-    // wordEl.classList = 'word'
-    // cardEl.appendChild(wordEl)
-
+    cardEl.addEventListener('click', e => {this.clickCard(e.target.dataset.id)})
+    cardEl.innerText = `${this.word.toUpperCase()}`
     return cardEl
   }
 
-  showColor(e){
-    const thisEl = document.querySelector(`div[data-id="${e.target.dataset.id}"]`)
+  clickCard(id){
+    this.clicked = true
+    this.showCard(id)
+  }
+
+  showCard(){
+    const thisEl = document.querySelector(`div[data-id="${this.id}"]`)
     thisEl.classList.add(this.type)
     thisEl.innerText = ""
   }
-
 
 }
